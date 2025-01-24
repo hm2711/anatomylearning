962d69152afbca36d6eef041d7451edd07633f51
hm2711 <hm.naa275@gmail.com>
2025-01-24 11:52:32 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+function myFunction1() {
+	alert('Teste tes connaissances!\n1) Les poumons sont un organe appartenant à quel appareil?\n2) Dans quelle région anatomique se situe cet organe?\n3) Quelles sont les fonctions assurées par les poumons?\n4) Cite 3 pathologies associées à cet organe.');
+}
+
+function envoyerMessage() {
+	var nom = document.getElementById("nom").value;
+	var prenom = document.getElementById("prenom").value;
+	
+	if (nom === "" || prenom === "") {
+		alert("Veuillez remplir tous les champs.");
+	}
+	else {
+		alert(`Merci ${prenom} ${nom}. Votre message a bien été envoyé.\nUne réponse vous sera apportée dans les 48h.`);
+	}
+}
+
+function monPseudo() {
+	let text;
+	let person = prompt("Veuillez entrer votre pseudo", "");
+	if (person == null || person == "") {
+		text = "Vous n'avez pas choisis de pseudo.";
+	} else {
+		text = "BIENVENUE " + person + " !";
+		}
+	document.getElementById("demo").innerHTML = text;
+}
+
+function myCheckbox() {
+	var checkBox = document.getElementById("cours6");
+	if (checkBox.checked == true){
+		text.style.display = "block";
+	} else {
+		text.style.display = "none";
+  }
+}
+
